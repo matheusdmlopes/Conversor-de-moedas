e56830defe1242d0f8c07f080da6af6f7764c147
maikonol <maikonoliveira056@gmail.com>
2024-11-20 02:33:24 -0300

    Primeiro comit - Conversor de Moedas

---
+++ script.js
@@ -0,0 +1,72 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")   //Valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value") // Outras Moedas 
+
+    const dolarToday = 5.8
+    const euroToday = 6.2
+    const libraToday = 7.4
+    const pargentinoToday = 0.0057
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-Uk", {
+            style: "currency",
+            currency: "GPB"
+        }).format(inputCurrencyValue / libraToday)
+    }
+    if (currencySelect.value == "peso-agentino") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("es-AR", {
+            style: "currency",
+            currency: "ARS"
+        }).format(inputCurrencyValue / pargentinoToday)
+    }
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+}
+
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImage.src="/assets/dolar.png"
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src="./assets/euro.png"
+    }  
+    
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "£ Libra"
+        currencyImage.src="./assets/libra.png"
+    }  
+
+    if (currencySelect.value == "peso-agentino") {
+        currencyName.innerHTML = "$ Peso Agentino"
+        currencyImage.src="./assets/peso argentino.png"
+    }  
+
+
+    convertValues()
+}
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
